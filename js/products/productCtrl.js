d3e3b844882003a8762236ae99a04a56e941535f
adriana <rubi12wera@gmail.com>
2015-07-27 21:49:58 -0600

    Initial commit

---
+++ js/products/productCtrl.js
@@ -0,0 +1,9 @@
+var app = angular.module('miniRouting');
+
+app.controller('productCtrl', function($scope, $routeParams, productService){
+  if($routeParams.id === 'shoes'){
+    $scope.productData = productService.shoeData;
+  } else if ($routeParams.id === 'socks'){
+    $scope.productData = productService.sockData;
+  }
+})
